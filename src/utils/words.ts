04b77db38043495d9eb25b37642a293c4a320018
10941096 <yuhuamou@boe.com.cn>
2023-03-30 16:43:34 +0800

    优化算法,添加更多自定义词,干掉无用两字母词

--- src/utils/words.ts
+++ src/utils/words.ts
@@ -3,4 +3,5 @@ import {readFileSync} from 'fs';
 // 加载英文词典
 const wordsFile = readFileSync('words_alpha.txt', 'utf8');
 export const words = new Set(wordsFile.split('\n').map(x => x.trim()));
-export const custom_dic = new Set(["wo", "pm", "rm", "sop", "list", "task", "state"]);
+
+export const custom_dic = new Set(["wo", "pm", "rm", "sop", "list", "task", "state", "quantity", "user", "description"]);
